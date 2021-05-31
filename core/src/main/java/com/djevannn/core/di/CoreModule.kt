2779c1142069430f1558c6a803bddcea5b8540b8
Evan <djevann11@gmail.com>
2021-05-31 18:20:09 +0700

    adding securities

--- core/src/main/java/com/djevannn/core/di/CoreModule.kt
+++ core/src/main/java/com/djevannn/core/di/CoreModule.kt
@@ -7,6 +7,9 @@ import com.djevannn.core.data.source.local.room.FoodDatabase
 import com.djevannn.core.data.source.remote.RemoteDataSource
 import com.djevannn.core.data.source.remote.network.ApiService
 import com.djevannn.core.domain.repository.IFoodRepository
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -18,16 +21,25 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<FoodDatabase>().foodDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("SUPER_SECRET".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             FoodDatabase::class.java,
             "Food.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
 val networkModule = module {
     single {
+        val hostname = "api.spoonacular.com"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/25Si5gmOzmVMiHQ9F+Ja6xBMK7u761/F87x9Ky/YsPY=")
+            .add(hostname, "sha256/DjBNmOWrmE91DLaH6gk+96MMBNjNs4/cbGxgvWLZi18=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(
                 HttpLoggingInterceptor().setLevel(
@@ -36,12 +48,14 @@ val networkModule = module {
             )
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.spoonacular.com/recipes/")
             .addConverterFactory(GsonConverterFactory.create())
+
             .client(get())
             .build()
         retrofit.create(ApiService::class.java)
